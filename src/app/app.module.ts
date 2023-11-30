9a3294db9d503f65ac63f6ccdaf539cf74b11ea6
jusish <ishimwejustin67@gmail.com>
2023-11-30 22:04:54 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponentComponent } from './nav-component/nav-component.component';
-import { SidenavComponentComponent } from './sidenav-component/sidenav-component.component';
-import { SidenavContentComponentComponent } from './sidenav-content-component/sidenav-content-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponentComponent,
-    SidenavComponentComponent,
-    SidenavContentComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/sidenav-content-component/sidenav-content-component.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-@Component({
-  selector: 'app-sidenav-content-component',
-  templateUrl: './sidenav-content-component.component.html',
-  styleUrls: ['./sidenav-content-component.component.css'],
-})
-export class SidenavContentComponentComponent {}
